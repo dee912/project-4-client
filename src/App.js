b6a9793a373fba7292a246d8cd51284b7f29f305
dee912 <devantespaine@gmail.com>
2021-06-12 18:59:40 +0100

    endpoint setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   React.useEffect(() => {
     const getData = async () => {
-      const res = await fetch('/api/endpoint') // * <-- replace with your endpoint
+      const res = await fetch('/api/stores')
       const data = await res.json()
       console.log(data)
     }
